8655a51a0f465fbc75d53b34418ff7b1cbec49f6
kumavis <aaron@kumavis.me>
2019-10-01 20:00:50 +0800

    animated lavamoat logo

--- src/presentation.js
+++ src/presentation.js
@@ -46,7 +46,7 @@ export default class Presentation extends React.Component {
         <Slide transition={['zoom']} bgColor="primary">
           <Image src="./lavamoat-logo.png" height="40vh"></Image>
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            LavaMoat
+          {makeAnimatedText()}
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             whats between the bad guys and your keys?
@@ -201,4 +201,33 @@ export default class Presentation extends React.Component {
       </Deck>
     );
   }
+}
+
+function makeAnimatedText () {
+  return (
+    // <div className="lavamoat-title-wrapper">
+      <svg className="lavamoat-title" viewBox="0 0 100 20">
+        <defs>
+          <linearGradient id="gradient" x1="0" x2="0" y1="0" y2="1">
+            <stop offset="5%" stop-color="red"/>
+            <stop offset="95%" stop-color="orange"/>
+          </linearGradient>
+          <pattern id="wave" x="0" y="0" width="120" height="20" patternUnits="userSpaceOnUse">
+            <path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" mask="url(#mask)" fill="url(#gradient)">
+              <animateTransform
+                  attributeName="transform"
+                  begin="0s"
+                  dur="6s"
+                  type="translate"
+                  from="0,0"
+                  to="40,0"
+                  repeatCount="indefinite" />
+            </path>
+          </pattern>
+        </defs>
+        <text text-anchor="middle" x="50" y="15" font-size="17" fill="url(#wave)"  fill-opacity="0.6">LavaMoat</text>
+        <text text-anchor="middle" x="50" y="15" font-size="17" fill="url(#gradient)" fill-opacity="0.2">LavaMoat</text>
+      </svg>
+    // </div>
+  )
 }
