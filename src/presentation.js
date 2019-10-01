b41adb839d945cfc7b9ab86b7d66844ee382af3b
kumavis <aaron@kumavis.me>
2019-10-01 17:07:42 +0800

    progress

--- src/presentation.js
+++ src/presentation.js
@@ -53,6 +53,27 @@ export default class Presentation extends React.Component {
           </Text>
         </Slide>
 
+        <Slide transition={['slide']} bgColor="primary">
+          <Heading size={1} textColor="secondary">
+            security
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            for any javascript app
+          </Heading>
+          <Heading size={6} textColor="tertiary">
+            but especially dapp UIs + wallets
+          </Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            event-stream
+          </Heading>
+          <Heading size={6} textColor="secondary">
+            a case study
+          </Heading>
+        </Slide>
+
         <Slide transition={['slide']} bgColor="secondary" textColor="primary">
           <Image style={{ display: 'inline-flex' }} className="anim-rotate-left" src="./npm-event-stream.png" height="40vh"></Image>
           <Image style={{ display: 'inline-flex' }} className="anim-rotate-left" src="./event-stream-article-0.png" height="40vh"></Image>
@@ -60,13 +81,72 @@ export default class Presentation extends React.Component {
           <Image style={{ display: 'inline-flex' }} className="anim-rotate-left" src="./event-stream-article-2.png" height="40vh"></Image>
         </Slide>
 
+        <Slide transition={['slide']} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            what are our options
+          </Heading>
+          <List>
+            <ListItem>no dependencies</ListItem>
+            <ListItem>audit all deps</ListItem>
+            <ListItem>...a third way?</ListItem>
+          </List>
+        </Slide>
+
         <Slide transition={['slide']} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Don't add security, remove insecurity</Quote>
+            <Quote>don't add security, remove insecurity</Quote>
             <Cite>Mark S. Miller</Cite>
           </BlockQuote>
         </Slide>
 
+        <Slide transition={['slide']} bgColor="primary">
+          <Heading size={1} textColor="secondary">
+            Agoric/SES
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            (Secure EcmaScript)
+          </Heading>
+          <Heading size={6} textColor="tertiary">
+            a secure runtime for running third-party code safely
+          </Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">
+            Frozen Intrinsics
+          </Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor="tertiary">
+          <Heading size={6} textColor="secondary">
+            ( problem, solution )
+          </Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor="secondary">
+          <Heading size={1} textColor="tertiary">
+            Explicit Endowments
+          </Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor="tertiary">
+          <Heading size={6} textColor="secondary">
+            ( problem, solution )
+          </Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor="tertiary">
+          <Heading size={6} textColor="secondary">
+            so how can we apply this to the event-stream incident?
+          </Heading>
+        </Slide>
+
+        <Slide transition={['zoom']} bgColor="primary">
+          <Image src="./lavamoat-logo.png" height="40vh"></Image>
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            LavaMoat
+          </Heading>
+        </Slide>
 
         <Slide transition={['slide']} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>
