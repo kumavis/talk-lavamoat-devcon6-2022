8c484c11276d851bb3b14373fb7861b690d24172
kumavis <aaron@kumavis.me>
2019-10-02 23:10:17 +0800

    add lavamoat-viz label

--- src/presentation.js
+++ src/presentation.js
@@ -255,6 +255,9 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide transition={['slide']} bgColor="tertiary">
+          <Heading size={6} textColor="secondary">
+            lavamoat-viz
+          </Heading>
           <iframe title="dep-graph viz" src="./dep-graph/index.html" style={{ width: '100%', height: '70vh', border: 0 }}></iframe>
         </Slide>
 
